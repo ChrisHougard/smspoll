a48f5a642c0609e5367d1cb0b5f0541ae4a7f322
Chris Hougard <chris@exchangecore.com>
2015-10-27 23:56:41 -0500

    Handle error

--- routes/poll.js
+++ routes/poll.js
@@ -5,11 +5,12 @@ var express = require('express'),
 router.get('/:id/', function(req, res, next) {
     poll.findById(req.params.id, function(err, poll) {
         if (err) {
-
+            console.log(err);
+            next();
         } else {
             poll.getPhoneNumber(function(err, phoneNum) {
                 if (err) {
-                    phoneNum = null
+                    phoneNum = null;
                 }
                 res.render('poll', { title: 'Poll Results', pollId: poll.data.id, pollName: poll.getDisplayName(), phoneNum: phoneNum });
             });
